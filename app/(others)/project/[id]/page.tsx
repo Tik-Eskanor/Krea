e0f0768a791e42b0e825c6ea50b19ea79aa098b3
Ade <tik.eskanor@gmail.com>
2025-07-10 08:52:41 +0100

    Push commit

--- app/(others)/project/[id]/page.tsx
+++ app/(others)/project/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getProject } from '@/lib/resource/project'
 import Image from 'next/image'
 import ProjectInfo from '@/components/custom/admin/ProjectInfo';
+import ImageGrid from '@/components/custom/ImageGrid';
 
 type Param = {
     params: Promise<{ id: string }>
@@ -16,36 +17,7 @@ export default async function page({ params }: Param) {
             <div className="absolute bg-gray-50 w-[550px] h-[550px] rounded-full border-2 border-gray-100 z-[-1] -bottom-70 -left-70 "></div>
             <div className='container px-2 mx-auto'>
                 <div className="flex gap-5 flex-wrap md:flex-nowrap my-5">
-                    <div className="w-full md:w-6/12">
-                        {projectData?.videoLink ? (
-                            <div>
-                                <iframe width="100%" height="300px" id="video" src={projectData.videoLink} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                                {projectData?.images.length ? (
-                                    <div className='flex justify-center gap-3 flex-wrap p-3 bg-gray-400'>
-                                        {projectData?.images.map((item) => (
-                                            <div key={item._id} className='basis-[47%] md:basis-[23%]'>
-                                                <Image src={item.url || ""} alt='' width={1000} height={1000} className='w-full object-cover rounded-md border border-white' />
-                                            </div>
-                                        ))}
-                                    </div>
-                                ) : ""}
-                            </div>
-                        ) : (
-                            <div>
-                                <Image src={projectData?.image.url || ""} alt='' width={1000} height={1000} className='w-full' />
-                                {projectData?.images.length ? (
-                                    <div className='flex justify-center gap-3 flex-wrap p-3 bg-gray-400'>
-                                        {projectData?.images.map((item) => (
-                                            <div key={item._id} className='basis-[47%] md:basis-[23%]'>
-                                                <Image src={item.url || ""} alt='' width={1000} height={1000} className='w-full object-cover rounded-md border border-white' />
-                                            </div>
-                                        ))}
-                                    </div>
-                                ) : ""}
-                            </div>
-                        )}
-
-                    </div>
+                    <ImageGrid projectData={projectData} />
                     <div className="w-full md:w-6/12">
                         <ProjectInfo projectData={projectData} />
                     </div>
