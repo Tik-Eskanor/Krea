c49eba2bf6300dcddeee9eb3122aed46afb062d2
Ade <tik.eskanor@gmail.com>
2025-07-10 10:12:30 +0100

    Push commit

--- components/custom/ImageGrid.tsx
+++ components/custom/ImageGrid.tsx
@@ -12,9 +12,6 @@ export default function ImageGrid({ projectData }: { projectData: Project | unde
             gallery: '#my-gallery',
             children: 'a',
             pswpModule: () => import('photoswipe'),
-            initialZoomLevel: 'fill', // Makes image fill viewport (height or width)
-            secondaryZoomLevel: 1.5,
-            maxZoomLevel: 2,
         });
 
         lightbox.init();
@@ -35,11 +32,11 @@ export default function ImageGrid({ projectData }: { projectData: Project | unde
                             {projectData?.images.map((item) => (
                                 <a key={item._id}
                                     href={item.url}
-                                    data-pswp-width="1024"
-                                    data-pswp-height="750"
+                                    data-pswp-width="100%"
+                                    // data-pswp-height="750"
                                     target="_blank"
                                     className='basis-[47%] md:basis-[23%] block'>
-                                    <Image src={item.url || ""} alt='' width={1000} height={1000} className='w-full object-cover rounded-md border border-white' />
+                                    <img src={item.url || ""} alt='' className='w-full object-cover rounded-md border border-white' />
                                 </a>
                             ))}
                         </div>
