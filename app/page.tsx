ec815adcfdc051693c4600d98f8cf43858a03247
Ade <tik.eskanor@gmail.com>
2025-09-15 02:39:56 +0100

    Push commit

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function page() {
                                     <div className={`p-[10px] rounded-xl ${data.color}`}><Icon size={23} className='text-white' /></div>
                                     <div>
                                         <div className='text-base md:text-sm font-semibold flex items-center gap-2 mb-[2px]'>
-                                            {data.title} {data.new && <span className='inline-block text-white bg-blue-700 rounded-[10px] px-2 py-[2px]'>New</span>}
+                                            {data.title} {data.new && <span className='inline-block text-white bg-blue-700 rounded-[11px] px-2 py-[2px]'>New</span>}
                                         </div>
                                         <div className='text-sm md:text-[12px] max-w-[180px]  md:max-w-[170px]'>{data.description}</div>
                                     </div>
